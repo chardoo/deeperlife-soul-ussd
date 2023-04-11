47d6287891197131a161cdf3a2a25b2e4c5af148
chardoo <40030004+chardoo@users.noreply.github.com>
2023-04-11 08:39:05 +0000

    first commit

---
+++ model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dataSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    gender: {
+        required: true,
+        type: String
+    },
+    contact: {
+        required: true,
+        type: String
+    },
+    town: {
+        required: true,
+        type: String
+    },
+});
+const Souls = mongoose.model("souls", dataSchema);
+
+module.exports = Souls;
