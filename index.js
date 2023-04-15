0ed847d47fdc477c7dd1d789291a8361441c1a25
asiedubrempong <kaybrempong@gmail.com>
2023-04-15 22:37:03 +0000

    remove * from regex

--- index.js
+++ index.js
@@ -111,10 +111,10 @@ menu.state("ageGroup.contact", {
     }
 
 
-    menu.con("Enter the phone number0:");
+    menu.con("Enter the phone number:");
   },
   next: {
-    "*0\d{9}":
+    "0\d{9}":
       "contact.town",
   },
 });
