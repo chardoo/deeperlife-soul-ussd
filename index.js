8c0d1d5b84ff9be709a55d74530cbd28f06606f6
asiedubrempong <kaybrempong@gmail.com>
2023-04-16 14:02:54 +0000

    check that number starts with 0

--- index.js
+++ index.js
@@ -113,9 +113,7 @@ menu.state("ageGroup.contact", {
     menu.con("Enter the phone number0:");
   },
   next: {
-    "*^\\d{10}$":
-    // "*[(]{0,1}[0-9]{3}[)]{0,1}[-s.]{0,1}[0-9]{3}[-s.]{0,1}[0-9]{4}$":
-      "contact.town",
+    "*^0\\d{9}$": "contact.town",
   },
 });
 
