f26cfdce93322143344bdeeac22bbda0225ecf6f
chardoo <40030004+chardoo@users.noreply.github.com>
2023-04-17 20:45:58 +0000

    cors update

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(helmet());
 // UssdMenu.
 
